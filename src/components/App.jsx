04beaa36758cd17b18921abd2d9d287a6af0a563
ArtemHryn <grynbest@gmail.com>
2022-10-06 01:57:44 +0300

    adding to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,32 @@ import { Contacts } from './Contacts/Contacts';
 import { ContactsFilter } from './ContactsFilter/ContactsFilter';
 import { Title, ContactTitle, Container } from './App.styled';
 
+const CONTACTS = 'contacts';
 
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(CONTACTS));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+}
+
+  componentDidUpdate(prevProp, prevState) {
+    console.log('2');
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    if (nextContacts !== prevContacts) {
+      console.log('1');
+      localStorage.setItem(CONTACTS, JSON.stringify(nextContacts));
+    }
+  }
+  
   onFormSubmit = ({ name, number }) => {
     const checkContactName = this.state.contacts.some(
       contact => contact.name === name
