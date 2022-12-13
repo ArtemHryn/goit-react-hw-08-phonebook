eca03a5b3b792da69439833cfcf247bb581b24a0
ArtemHryn <grynbest@gmail.com>
2022-12-13 10:25:02 +0200

    routing for gh

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
